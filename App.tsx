5bbfe18175fbcde7740f3d26dec72173168d2604
Priestly <priestlyadejo@gmail.com>
2023-08-05 00:14:31 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Hello poop World</Text>
+    </View>
+  );
+};
+
+export default App;
